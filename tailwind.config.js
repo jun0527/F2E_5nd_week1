6edea7e7d2be514c992d9859feb909c1a877cc4f
yijun <sandy3068@yahoo.com.tw>
2023-11-11 23:17:27 +0800

    feat: create vue project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: "#00A199",
+      primary_light: "#14C3BB",
+      secondary: "#FFDC9E",
+      secondary_dark: "#F4CA80",
+      secondary_light: "#FFE9C3",
+      black: "#515151",
+      white: "#FFFFFF"
+    },
+    extend: {
+      fontFamily: {
+        Gaegu: "Gaegu",
+        NotoSansTc: "Noto Sans TC",
+        NotoSerifTc: "Noto Serif TC"
+      }
+    }
+  },
+  plugins: []
+};
